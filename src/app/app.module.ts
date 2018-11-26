ceae13591fcab8d148146c89a70964ec36881535
Saravanan <saravanang@syncfusion.com>
2018-11-26 12:57:19 +0530

    Add files via upload
    Angular 5 files moved

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { RichTextEditorAllModule } from '@syncfusion/ej2-angular-richtexteditor';
+
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, RichTextEditorAllModule, 
+  ],
+
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
